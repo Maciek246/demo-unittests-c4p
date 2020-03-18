e5dc2eb21c9d5fb166a9c5206c52472861ace73e
Maciej <maciej.matuszczak96@gmail.com>
2020-03-18 19:54:34 +0100

    Cleaning up

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -4,34 +4,37 @@ import { render, fireEvent } from "@testing-library/react";
 
 describe('Button component', () => {
     it('should be rendered', () => {
-        const { queryByText } = render(<Button>TEST</Button>)
+        const { container } = render(<Button />)
 
-        expect(queryByText('TEST')).toBeTruthy() // or toBeInTheDocument()
+        expect(container.firstChild).toBeInTheDocument()
     });
 
     it('should render children prop as label', () => {
         const component = render(<Button>TEST</Button>);
-        expect(component.queryByText('TEST')).toBeTruthy();
+
+        expect(component.queryByText('TEST')).toBeInTheDocument();
     });
 
     it('should call function given as prop by clicking button', () => {
         const mock = jest.fn();
+        const component = render(<Button onClick={mock}>TEST</Button>);
 
-        const component = render(<Button onClick={mock}>TEST</Button>)
-        fireEvent.click(component.queryByText('TEST'))
+        fireEvent.click(component.queryByText('TEST'));
 
         expect(mock).toHaveBeenCalledTimes(1);
     });
 
     it('should display extra paragraph if `extraParagraph` prop is set', () => {
-        const component = render(<Button extraParagraph={true}>TEST</Button>)
+        const component = render(<Button extraParagraph={true}>TEST</Button>);
 
-        expect(component.queryByText('Extra paragraph')).toBeTruthy();
-    })
+        expect(component.queryByText('Extra paragraph')).toBeInTheDocument();
+    });
 
     it('should not display extra paragraph if `extraParagraph` prop is falsy', () => {
-        const component = render(<Button extraParagraph={false}>TEST</Button>)
+        const component = render(<Button extraParagraph={false}>TEST</Button>);
 
         expect(component.queryByText('Extra paragraph')).toBeNull();
-    })
+
+    });
 })
+
